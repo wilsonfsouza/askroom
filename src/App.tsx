f9d18225aea53d47570050fbb770d68d3d78cac1
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-22 19:45:13 -0700

    feat: base implementation of react-router-dom with home and new room pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
