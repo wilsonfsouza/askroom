fc23c81438a9c2d1b7b24dcbf7a4b0d2a00f8e63
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-22 20:25:02 -0700

    feat: got user from signIn and passed both user and signInWithGoogle to the context.

--- src/contexts/AuthenticationContext.tsx
+++ src/contexts/AuthenticationContext.tsx
@@ -1,7 +1,15 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useCallback, useState } from 'react';
+import { firebase, auth } from '../services/firebase';
+
+type User = {
+    id: string;
+    name: string;
+    avatar: string;
+}
 
 interface AuthenticationContextData {
-    user: object;
+    user: User | undefined;
+    signInWithGoogle: () => void;
 }
 
 interface AuthenticationProviderProps {
@@ -11,11 +19,35 @@ interface AuthenticationProviderProps {
 export const AuthenticationContext = createContext<AuthenticationContextData>({} as AuthenticationContextData);
 
 export function AuthenticationProvider({ children }: AuthenticationProviderProps) {
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState<User>();
+
+    const signInWithGoogle = useCallback(() => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result => {
+            if (result.user) {
+                const { displayName, photoURL, uid } = result.user;
+
+                if (!displayName || !photoURL) {
+                    throw new Error('Missing information from Google Account.')
+                }
+
+                const formatedUser = {
+                    id: uid,
+                    name: displayName,
+                    avatar: photoURL,
+                }
+
+                setUser(formatedUser);
+            }
+            console.log(result);
+        });
+    }, []);
 
     return (
         <AuthenticationContext.Provider value={{
-            user
+            user,
+            signInWithGoogle
         }}>
             { children}
         </AuthenticationContext.Provider>
