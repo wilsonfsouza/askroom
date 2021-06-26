b17ea5fb76a90e07e825e52efaa6b2b3e6402089
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-26 16:46:57 -0700

    feat: created room page.

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,27 @@
+import { Button } from '../components/Button';
+import { Header } from '../components/Header';
+import styles from '../styles/pages/room.module.scss';
+
 export function Room() {
     return (
-        <h1>Room</h1>
+        <div>
+            <Header />
+            <main className={styles.content}>
+                <div className={styles.roomTitle}>
+                    <h1>Room React</h1>
+                    <span>4 questions</span>
+                </div>
+
+                <form className={styles.form}>
+                    <textarea
+                        placeholder="What would you like to know?"
+                    />
+                    <div className={styles.formFooter}>
+                        <span><button>Sign in</button> to send a question</span>
+                        <Button type="submit">Send a question</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
