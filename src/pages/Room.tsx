e855c727d6e7ebf37e7f57289eea1e7e7353effc
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-26 19:18:57 -0700

    feat: added user flow to add a new question.

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,10 @@
+import { FormEvent, useState } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
+import { useAuthentication } from '../hooks/useAuthentication';
+import { database } from '../services/firebase';
 import styles from '../styles/pages/room.module.scss';
 
 type RoomParams = {
@@ -8,25 +12,74 @@ type RoomParams = {
 }
 
 export function Room() {
+    const { user } = useAuthentication();
     const params = useParams<RoomParams>();
+    const roomId = params.id;
+
+    const [newQuestion, setNewQuestion] = useState('');
+
+    async function handleSendNewQuestion(event: FormEvent) {
+        event.preventDefault();
+
+        if (newQuestion.trim() === '') {
+            return;
+        }
+
+        if (!user) {
+            toast.error('You must be logged in', {
+                position: 'top-center'
+            })
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                avatar: user?.avatar,
+                name: user?.name
+            },
+            isHighlighted: false,
+            isAnswered: false
+        };
+
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+        toast.success('Your question was added to the list', {
+            position: 'top-center'
+        })
+
+        setNewQuestion('');
+    }
 
     return (
         <div>
-            <Header code={params.id} />
+            <Header code={roomId} />
             <main className={styles.content}>
                 <div className={styles.roomTitle}>
                     <h1>Room React</h1>
                     <span>4 questions</span>
                 </div>
 
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleSendNewQuestion}>
                     <textarea
                         placeholder="What would you like to know?"
+                        onChange={(event) => setNewQuestion(event.target.value)}
+                        value={newQuestion}
                     />
                     <div className={styles.formFooter}>
-                        <span><button>Sign in</button> to send a question</span>
-                        <Button type="submit">Send a question</Button>
+                        {user
+                            ? (
+                                <div className={styles.userInfo}>
+                                    <img src={user.avatar} alt={user.name} />
+                                    <span>{user.name}</span>
+                                </div>
+                            )
+                            : (<span><button>Sign in</button> to send a question</span>)}
+                        <Button type="submit" disabled={!user} style={{ maxWidth: '12rem' }}>Send a question</Button>
                     </div>
+                    <Toaster
+                        position="top-center"
+                        reverseOrder={false}
+                    />
                 </form>
             </main>
         </div>
