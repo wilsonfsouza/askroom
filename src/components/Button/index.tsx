bc4c9bc259abbe785c1529b613e6c7e877f1b6ef
Wilson Franca <wilson.franca.92@gmail.com>
2021-07-27 21:50:19 -0500

    refactor: added prop for outlined version of button and conditional for admin room.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,13 +3,15 @@ import { IconBaseProps } from "react-icons";
 import styles from "./styles.module.scss";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    leftIcon?: React.ComponentType<IconBaseProps>;
     children: string;
+    leftIcon?: React.ComponentType<IconBaseProps>;
+    isOutlined?: boolean;
 }
 
-export function Button({ leftIcon: Icon, children, ...rest }: ButtonProps) {
+export function Button({ leftIcon: Icon, isOutlined = false, children, ...rest }: ButtonProps) {
+    const outlined = isOutlined ? styles.outlined : ''
     return (
-        <button type="button" className={styles.button} {...rest}>
+        <button type="button" className={`${styles.button} ${outlined}`} {...rest}>
             {Icon && <Icon className={styles.buttonLeftIcon} />}
             {children}
         </button>
