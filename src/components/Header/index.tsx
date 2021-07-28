298e81d247d8540cd54af4658019dcdd52aa10cf
Wilson Franca <wilson.franca.92@gmail.com>
2021-07-27 21:51:10 -0500

    refactor: added button for admin room.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
 import styles from './styles.module.scss';
 import logoImg from '../../assets/images/logo.svg';
 import { RoomCode } from '../RoomCode';
+import { Button } from '../Button';
 
 interface HeaderProps {
     code: string;
+    isAdminRoom?: boolean;
 }
 
-export function Header({ code }: HeaderProps) {
+export function Header({ code, isAdminRoom }: HeaderProps) {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src={logoImg} alt="Askroom" />
-                <RoomCode code={code} />
+                <div>
+                    <RoomCode code={code} />
+                    {isAdminRoom && (<Button style={{ width: '10rem', height: '2.5rem' }} isOutlined>Close Room</Button>)}
+                </div>
             </div>
         </header>
     );
